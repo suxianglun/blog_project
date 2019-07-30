f1083ebe282b6169bf567a50ef58da6e6db1d969
suxianglun <suxianglun@163.com>
2019-07-30 19:48:22 +0800

    first commit code

---
+++ CHANGE_LOG.md
@@ -0,0 +1,8 @@
+## 更新日志
+#### 2019-7-19
+  - [x] 修复:
+  
+  - [x] 添加: sass文件支持 django-compressor django-sass-processor
+  - [x] 配置: 配置项目settings: static文件路径
+  - [x] 添加：导入前端模板
+  - [x] 创建：创建项目及其配置
---
+++ blog_project_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/suxianglun/Downloads/project/python/blog_project/blog_project_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

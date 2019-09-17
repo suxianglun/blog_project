9a360778633fd84e67c17f7a95115290606d50f3
suxianglun <suxianglun@163.com>
2019-09-17 19:39:29 +0800

    update .gitignore

--- blog_project_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/suxianglun/Downloads/project/python/blog_project/blog_project_env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
